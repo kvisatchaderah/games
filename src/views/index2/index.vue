153df61068f015f885dde3dcd7d26b29b9fc6876
kvisatchaderah <kvisatchaderah@gmail.com>
2022-12-13 12:15:34 +0300

    ttt

--- src/views/index2/index.vue
+++ src/views/index2/index.vue
@@ -1,14 +1,37 @@
 <template>
-  <div class="index">
-    {{ index }}
+  <ListMaker :prop_function="add_list_item" />
+
+  <div
+    v-for="list_item in list"
+    :key="list_item.id"
+  >
+    <hr />
+    {{ list_item.id }} || {{ list_item.text }}
   </div>
 </template>
 
-<style scoped lang="sass">
-@import 'index.sass'
-</style>
-
 <script lang="ts">
-import script from './index'
-export default script
+import { defineComponent } from 'vue'
+import ListMaker from './list_maker.vue'
+import list_item from './list_item'
+
+export default defineComponent({
+  components: { ListMaker },
+
+  data() {
+    return {
+      index: 'data index 2',
+      list: [] as list_item[],
+    }
+  },
+
+  methods: {
+    add_list_item(text: string) {
+      this.list.push({
+        id: Math.round(1000 * Math.random()),
+        text: text,
+      })
+    },
+  },
+})
 </script>
